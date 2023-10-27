6d0f2a3d1cc30cab38b636367f9536c10a5ab5a0
Jacek <jacek_kozakowski_dev@wp.pl>
2023-10-27 21:07:24 +0200

    Add button

--- to-do_list.js
+++ to-do_list.js
@@ -14,12 +14,24 @@ let list = [{
 
 const add = `<li><button class="add">+</button></li>`;
 const toDoList = document.querySelector(".list");
-let listContent = "";
 
-list.forEach((task) => {
-  listContent += `
-    <li><button class="check check-${task.id}"><img src="check-mark-button.png" alt="check" width="60px" height="60px" class="hidden"></button><p class="content">${task.content}</p><button class="remove remove-${task.id}">x</button></li>
-  `;
-})
-toDoList.innerHTML = listContent + add;
+const actualise = () => {
+  let listContent = "";
+  list.forEach((task) => {
+    listContent += `
+      <li><button class="check"><img src="check-mark-button.png" alt="check" width="60px" height="60px" class="hidden"></button><p class="content">${task.content}</p><button class="remove">x</button></li>
+    `;
+  });
+  toDoList.innerHTML = listContent + add;
 
+  const addButton= document.querySelector('.add');
+  addButton.addEventListener("click", (event) => {
+    list.push({
+      id: list.length+1,
+      content: `task${list.length+1}`
+    });
+    actualise();
+  });
+}
+
+actualise();
